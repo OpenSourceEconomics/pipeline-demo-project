9e0d34d2590fcebd308cad40d57a30c5e6e83dce
Tobias Raabe <raabe@posteo.de>
2020-03-31 14:20:31 +0200

    Fixed workflows for v0.0.3.

--- simple-project/README.rst
+++ simple-project/README.rst
@@ -5,5 +5,6 @@ simple-project
 This workflow does the following:
 
 - Create random data sets.
-- Run OLS and Logit regression in R and Python on the data.
+- Run OLS, Logit, Probit, ordered Logit, and ordered Probit regressions in R and Python
+  on the data.
 - Report the results in regression tables generated with R and Python.
--- simple-project/src/create_random_data_ordered_y.r
+++ simple-project/src/create_random_data_ordered_y.r
@@ -9,8 +9,8 @@ x1    <- rnorm(n, 175, 7)
 x2    <- rnorm(n,  30, 8)
 y_continuous <- 0.5 * x1 - 0.3 * x2 + 10 + rnorm(n, 0, 6)
 y_ordinal  <- cut(y_continuous, breaks=quantile(y_continuous), include.lowest=TRUE,
-             labels=c("--", "-", "+", "++"), ordered=TRUE)
+              labels=c("--", "-", "+", "++"), ordered=TRUE)
 
-df <- data.frame(x1 = x1, x2 = x2, y = factor(y_ordinal))
+df <- data.frame(x1 = x1, x2 = x2, y = as.factor(y_ordinal))
 
 save_data(df, "{{ produces }}")
